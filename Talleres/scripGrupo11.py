24e39c2572987b199223fb11c3b1d4bcc4ca5315
Guillermo Jimenez <jimenezguillermo1208@gmail.com>
2020-11-11 00:10:30 -0400

    agrego otra cosa

--- Talleres/scripGrupo11.py
+++ Talleres/scripGrupo11.py
@@ -2,24 +2,25 @@
 def genCiclosHamiltoneanos(G,nodo,lista,nodos,nodoInicial):  
     cont = 0
     for i in G[nodo]:
-        if i:
-            if cont not in lista and cont != nodos:
-                if cont not in lista or len(lista) == 3:
-                    if nodo not in lista:
-                        lista.append(nodo)
-                    next(genCiclosHamiltoneanos(G,cont,lista,nodos,nodoInicial))
+        if len(lista) < (len(G)-1):
+            if i:
+                if cont not in lista and cont != nodos:
+                    if cont not in lista or len(lista) == 3:
+                        if nodo not in lista:
+                            lista.append(nodo)
+                        nodos = -1
+                        next(genCiclosHamiltoneanos(G,cont,lista,nodos,nodoInicial))
+                else:
+                    cont +=1
             else:
                 cont +=1
-        else:
-            cont +=1
+        #else:
     if len(lista) != len(G):
         nodos = lista.pop()
         next(genCiclosHamiltoneanos(G,lista[len(lista)-1],lista,nodos,nodoInicial))
     else:
         yield lista
 
-
-       
 
 G = [
     [False,True,True,False,True],
