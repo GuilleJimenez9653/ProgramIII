cbb8a56e74698ff8f9a064f2d4cb5adcf3610788
Guillermo Jimenez <jimenezguillermo1208@gmail.com>
2020-11-12 11:13:31 -0400

    hace la primera cadena pero da error en la linea 12 por algun motivo gracias al next

--- Talleres/scripGrupo11.py
+++ Talleres/scripGrupo11.py
@@ -2,7 +2,7 @@
 def genCiclosHamiltoneanos(G,nodo,lista,nodos,nodoInicial):  
     cont = 0
     for i in G[nodo]:
-        if len(lista) < (len(G)-1):
+        if len(lista) < (len(G)-1) or nodo == nodoInicial:
             if i:
                 if cont not in lista and cont != nodos:
                     if cont not in lista:
@@ -14,10 +14,17 @@ def genCiclosHamiltoneanos(G,nodo,lista,nodos,nodoInicial):
                     cont +=1
             else:
                 cont +=1
-        #else:
+        #else:       
     if len(lista) != len(G):
-        nodos = lista.pop()
-        next(genCiclosHamiltoneanos(G,lista[len(lista)-1],lista,nodos,nodoInicial))
+        nodoFinal = nodo
+        posicionFinal = G[nodoFinal]
+        posicionInicial =G[nodoInicial]
+        if posicionFinal[nodoInicial] == True and posicionInicial[nodoFinal] == True and len(lista) == (len(G)-1):
+            lista.append(nodo)
+            yield lista
+        else:
+            nodos = lista.pop()
+            next(genCiclosHamiltoneanos(G,lista[len(lista)-1],lista,nodos,nodoInicial))
     else:
         yield lista
 
