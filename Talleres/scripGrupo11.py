b1ea5d29de2817ebd71a7b10b693c8e535139fa0
Guillermo Jimenez <jimenezguillermo1208@gmail.com>
2020-11-13 12:45:32 -0400

    funciona

--- Talleres/scripGrupo11.py
+++ Talleres/scripGrupo11.py
@@ -1,5 +1,5 @@
 #Taller 1 
-def genCiclosHamiltoneanos(G,nodo,lista,nodos,nodoInicial):  
+def genCiclosHamiltoneanos1(G,nodo,lista,nodos,nodoInicial,listaAux):  
     cont = 0
     for i in G[nodo]:
         if len(lista) < (len(G)-1) or nodo == nodoInicial:
@@ -9,7 +9,7 @@ def genCiclosHamiltoneanos(G,nodo,lista,nodos,nodoInicial):
                         if nodo not in lista:
                             lista.append(nodo)
                         nodos = -1
-                        yield from genCiclosHamiltoneanos(G,cont,lista,nodos,nodoInicial)
+                        yield from genCiclosHamiltoneanos1(G,cont,lista,nodos,nodoInicial,listaAux)
                 else:
                     cont +=1
             else:
@@ -22,10 +22,33 @@ def genCiclosHamiltoneanos(G,nodo,lista,nodos,nodoInicial):
             yield lista
         else:
             nodos = lista.pop()
-            yield from genCiclosHamiltoneanos(G,lista[len(lista)-1],lista,nodos,nodoInicial)
+            yield from genCiclosHamiltoneanos1(G,lista[len(lista)-1],lista,nodos,nodoInicial,listaAux)
     else:
         yield lista
 
+def genCiclosHamiltoneanos(G,nodo,camino,nodoInicial):
+    cont = 0
+    for i in G[nodo]:
+        if esHamiltoneano(G,camino,cont,nodoInicial) and cont not in camino and cont < len(G):
+            yield camino + [cont]
+        cont += 1
+    cont = 0
+    for i in G[nodo]:
+        if G[nodo] and cont not in camino and i and cont < len(G) and esAdyacente(G,camino[len(camino)-1],cont):
+            yield from genCiclosHamiltoneanos(G,cont,camino + [cont],nodoInicial)
+        cont += 1
+
+def esHamiltoneano(G,camino,nodo,nodoInicial):
+    posicionFinal = G[nodo]
+    posicionInicial =G[nodoInicial]
+    if len(camino) == (len(G)-1) and posicionFinal[nodoInicial]==True and posicionInicial[nodo]== True:
+        return True
+
+def esAdyacente(G,nodo1,nodo2):
+    posicionNodo1 = G[nodo1]
+    posicionNodo2 = G[nodo2]
+    if posicionNodo1[nodo2] == True and posicionNodo2[nodo1] == True:
+        return True
 
 G = [
     [False,True,True,False,True],
@@ -36,7 +59,10 @@ G = [
 ]
 
 listaAux = []
+listaAux2 = []
 nodoFinal = -1
-iterador = genCiclosHamiltoneanos(G,0,listaAux,nodoFinal,0)
-print(next(iterador))
-print(next(iterador))
+#iterador = genCiclosHamiltoneanos1(G,0,listaAux,nodoFinal,0,listaAux2)
+#print(next(iterador))
+
+gen = genCiclosHamiltoneanos(G,0,[0],0)
+print(list(gen))
